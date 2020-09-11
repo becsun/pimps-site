7e60cf6fa4061af315a6585984855c1a8bbc59d0
Bec Sun <rebeccasun86@gmail.com>
2020-09-11 01:27:18 +0100

    added react frontend and installed npm. Changed the border colours too so it wontbe mixed up. Changed the proxymiddleware code so now just needs to link up to the backend urls when they are done

--- staff/models.py
+++ staff/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 class Staff(models.Model):
     name = models.CharField(max_length=30, unique=True)
+    role = models.CharField(max_length=30, null=True)
     bio = models.CharField(max_length=200)
     image = models.CharField(max_length=30)
-    fav_cut = models.CharField(max_length=30)
+    favcut = models.CharField(max_length=30, null=True)
     # services = models.ManyToMany(
     #     'services.Service',
     #     related_name='staffs'
