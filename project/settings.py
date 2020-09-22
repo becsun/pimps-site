ebcd5aa06eb6c41eb0978b4d40196735ffca7ad3
Bec Sun <rebeccasun86@gmail.com>
2020-09-22 23:23:08 +0100

    final changes

--- project/settings.py
+++ project/settings.py
@@ -132,7 +132,7 @@ ROOT_URLCONF = 'project.urls' #check if you have this already, if not add it in
 STATIC_URL = '/static/' # same with this
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'frontend', "public"),
+    os.path.join(BASE_DIR, 'frontend', "build", "static"),
 )
 
 django_heroku.settings(locals())
