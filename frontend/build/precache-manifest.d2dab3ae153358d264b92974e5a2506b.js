df632fdf4e01e44fbc7074ebf56c72b4d9f9f0d6
Bec Sun <rebeccasun86@gmail.com>
2020-09-22 23:27:27 +0100

    heeeelp

---
+++ frontend/build/precache-manifest.d2dab3ae153358d264b92974e5a2506b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "216c5a646623aaa71a43f770f18220d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f92df966e78d62e65a64",
+    "url": "/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "008f52ad6e594c380be0",
+    "url": "/static/css/main.57139b57.chunk.css"
+  },
+  {
+    "revision": "f92df966e78d62e65a64",
+    "url": "/static/js/2.3e113cc7.chunk.js"
+  },
+  {
+    "revision": "33ade7f87243ebea1c83e915f94afce3",
+    "url": "/static/js/2.3e113cc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "008f52ad6e594c380be0",
+    "url": "/static/js/main.8e537724.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "e7d11ebdf7417cdeb4335796e4a0ba76",
+    "url": "/static/media/banner-logopng__567x417_q85_crop_upscale.e7d11ebd.png"
+  },
+  {
+    "revision": "a231a862765c192bc1d0cd5e1ca6b09c",
+    "url": "/static/media/homepage_edit_wliqge.a231a862.jpg"
+  }
+]);
